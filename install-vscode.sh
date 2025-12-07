8baaf8fed62846f4d651759ba2a00c8fc53ca422
rich <rfhart514@gmail.com>
2025-12-07 14:14:34 -0500

    initial scripts push

---
+++ install-vscode.sh
@@ -0,0 +1,6 @@
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
---
+++ packages.sh
@@ -0,0 +1,3 @@
+apt update && apt upgrade -y
+apt install -y curl
+
